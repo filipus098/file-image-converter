1dac6f9ef4bb4f3b6a5737dfcb899dfabd0f01d1
filipus098 <filipus098@gmail.com>
2019-10-03 20:36:45 +0200

    added threads

--- Prog/main.py
+++ Prog/main.py
@@ -1,50 +1,79 @@
-import bitio
+
 import os
-from PIL import Image
+import threading
+import time
+
 import numpy as np
-import scipy.misc as smp
 import pandas as pd
-print("NOTE: you need to have a few python packages installed for this to worked, PIL (or pillow) numpy scipy and pandas")
+from PIL import Image
+
+
+def showProgress():
+    global threadsStarted
+    while (int(threadsStarted / int(leng)) * 100) < 100:
+        print(str(int((threadsStarted / int(leng)) * 100)) + "% threads started")
+        time.sleep(1)
+
+
+def evalPxl(i):
+    j = 0
+    toBinString = string[i:i + 4]
+    i = i / 4
+    while i >= h:
+        j += 1
+        i = i - w
+    value = int(toBinString, 2)
+    data[j, int(i)] = [coloursFrame.iloc[value]["R"], coloursFrame.iloc[value]["G"], coloursFrame.iloc[value]["B"]]
+
+
+print("NOTE: you need to have a few python packages installed for this to worked, PIL (or pillow) numpy and pandas")
 print("Please put in the path to your file (relative or full both work)")
-fileName=input()
+fileName = input()
 coloursFrame = pd.read_csv("pxls.space palette.csv")
-print(coloursFrame)
 
 f = open(fileName, 'rb')
 
 # read one byte
-string=""
+string = ""
 # convert the byte to an integer representation
+# TODO Threads would be good but effort
 for i in range(os.path.getsize(fileName)):
     byte = f.read(1)
     print(str(int(i/os.path.getsize(fileName)*100)) + ' % byte converted')
     try:
-     byte = ord(byte)
+        byte = ord(byte)
     except:
-     byte= 0
+        byte = 0
     # now convert to string of 1s and 0s
     byte = bin(byte)[2:].rjust(8, '0')
     # now byte contains a string with 0s and 1s
-    #TODO maybe a better solution
+    #TODO a better solution
     for bit in byte:
-        string +=bit
+        string += bit
 w, h = int(np.ceil(np.sqrt(len(string)/4))),int(np.ceil(np.sqrt(len(string)/4)))
 data = np.zeros((h, w, 3), dtype=np.uint8)
-j=0
+
 #TODO performance improvement
+
+threadsStarted = 0
+leng = int(len(string) / 4)
+progress = threading.Thread(target=showProgress)
+progress.start()
+
 for i in range(0,len(string),4):
-    toBinString = string[i:i+4]
-    value = int(toBinString,2)
-    print(str(int((i+1)*100/len(string))) + " % done")
-    if(i/4-j*w>=h):
-        j+=1
+    curr = threading.Thread(target=evalPxl, args=[i])
+    curr.start()
+    threadsStarted += 1
 
-    data[j,int(i/4)-j*w]=[coloursFrame.iloc[value]["R"],coloursFrame.iloc[value]["G"],coloursFrame.iloc[value]["B"]]
-
-img = Image.fromarray(data,'RGB')       # Create a PIL image
+while threading.active_count() != 1:
+    print(str(int(threading.active_count())) + " threads alive")
+    time.sleep(2)
+img = Image.fromarray(data, 'RGB')  # Create a PIL image
 
 print("where do you want your image saved (just the path)?")
 print("WARNING if you have another image.png in your path its going to get overriden")
 img.save(input()+"image.png")
 img.show()
-print("Done! the picture view should pop up and show up in your path, as a tip  recommend viewing it in paint or other editing programms because upscaling works weirdly with small pictures")
+print("Done! the picture view should pop up and show up in your path,"
+      " as a tip  recommend viewing it in paint or other editing programms "
+      "because upscaling works weirdly with small pictures")
